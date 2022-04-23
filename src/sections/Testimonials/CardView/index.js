3df960fb56824b2e3bb59c57c83fdc890865b1e4
Isaac Fernandes <isaacwritescode@protonmail.com>
2022-04-23 23:02:42 +0530

    responsive testimonial cards

--- src/sections/Testimonials/CardView/index.js
+++ src/sections/Testimonials/CardView/index.js
@@ -8,10 +8,30 @@ export const CardView = forwardRef(({ b1 }, ref) => {
     duration: 3000,
     transitionDuration: 300,
     easing: "ease",
-    slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
+    slidesToShow: 1,
     arrows: false,
+    responsive: [
+      {
+        breakpoint: 1372,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
   };
   return (
     <Slide {...properties} ref={ref}>
