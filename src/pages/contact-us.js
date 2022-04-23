09b46650e68f948c0e2939ee3cfb6631cd29ab1c
Isaac Fernandes <isaacwritescode@protonmail.com>
2022-04-23 21:31:12 +0530

    netilify form test

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -52,7 +52,7 @@ export const ContactUs = () => {
           </Stack>
         </Grid>
         <Grid item lg={7} width="100%">
-          <form name="contact" method="POST" data-netlify="true">
+          <form name="contact" action="POST" data-netlify="true">
             <Stack
               p={6}
               spacing={4}
